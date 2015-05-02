aa7b64d2c8e52c87446ad96b65a77ef3baf9e816
nwparker <nwp7@cornell.edu>
2015-05-01 22:07:16 -0400

    run this on the split files

--- data_collection/video_data.py
+++ data_collection/video_data.py
@@ -44,9 +44,8 @@ def get_approx_score(vid_id, score=None, next_page_tok=None):
 	# Process the thread and get the associated comments
 	for item in api_results["items"]:
 		num_replies = item["snippet"]["totalReplyCount"]
-		# Only if this has 2 or more replies do I add it
-		if num_replies >= 2:
-			score += num_replies
+		# Only add for 2 or more replies
+		score = score + num_replies if num_replies >= 2 else score
 	# Recur
 	new_next_page_tok = api_results["nextPageToken"] if "nextPageToken" in api_results else ''
 	return get_approx_score(vid_id, score, new_next_page_tok)
@@ -106,7 +105,7 @@ def get_comment_threads(vid_id, data=None, next_page_tok=None):
 	return get_comment_threads(vid_id, data, new_next_page_tok)
 
 
-def get_video_data(vid_id, categories_list):
+def get_video_data(vid_id, categories_list, approx=True):
 	""" Gets all the relevant data for a video """
 	video_info_list = youtube.videos().list(
 		part="topicDetails, snippet, contentDetails, statistics",
@@ -122,48 +121,59 @@ def get_video_data(vid_id, categories_list):
 	topics 			= video_info["topicDetails"]["topicIds"] if 'topicDetails' in video_info and 'topicIds' in video_info["topicDetails"] else [] #code around bug
 	captions 		= get_formatted_transcript(vid_id)
 
+	if num_views < 100000: #Optimization by doing this check also throws away videos with bad captions
+		return None
 	if captions == None: #Optimization by doing this check also throws away videos with bad captions
 		return None
-	comment_thread = get_comment_threads(vid_id)
 	vid_data = {
 		"title" 				: title,
 		"video_length" 			: duration_string,
-		"vide_defintion"		: video_defintion,
+		"video_defintion"		: video_defintion,
 		"number_views" 			: num_views,
 		"publish_date" 			: publish_date,
 		"topics" 				: topics,
 		'categories'			: categories_list,
 		"captions" 				: captions,
-		'comment_thread'		: comment_thread
 	}
+	if approx:
+		raw_score 				= get_approx_score(vid_id) 
+		vid_data['raw_score'] 	= raw_score
+		vid_data['score']		= (raw_score/float(num_views))
+	else:
+		vid_data['comment_thread'] = get_comment_threads(vid_id)
+
 	return vid_data
 
-def process_inv_idx(inv_idx, cautious=False, path=None):
+def process_inv_idx(inv_idx, path=None, cautious=True):
 	vid_id_set = set(get_filenames(path))
 	all_data = {}
 	for vid_id, categories_list in inv_idx.iteritems():
 		if vid_id not in vid_id_set:
 			print "proccessing: ", vid_id
 			cur_vid_data = get_video_data(vid_id, categories_list)
-			if cautious:
-				with open(path+vid_id+'.json', 'w') as datafile:
-					json.dump(cur_vid_data, datafile, indent = 4, ensure_ascii=True)
-			else:
-				all_data[vid_id] = cur_vid_data
+			if cur_vid_data is not None:
+				if cautious:
+					with open(path+vid_id+'.json', 'w') as datafile:
+						json.dump(cur_vid_data, datafile, indent = 4, ensure_ascii=True)
+				else:
+					all_data[vid_id] = cur_vid_data
 	return all_data
+
+
 
 def try_forever():
 	try:
-		videos_data = process_inv_idx(videoIds_inv_idx, cautious=True, path="comments/")
+		process_inv_idx(videoIds_inv_idx, path="comments_v2/", cautious=True)
 	except:
 		try_forever()
 
 
 if __name__ == "__main__":
 	os.chdir(os.path.join(os.pardir, 'data')) #go into data folder
-	input_json_filename = 'video_ids_v7_pruned_pruned.json'
+	input_json_filename = 'video_ids_v5.json'
 	videoIds_inv_idx = json.load(open(input_json_filename))
-	try_forever()
+	# try_forever()
+	process_inv_idx(videoIds_inv_idx, path="comments_v2/", cautious=True)
 
 	# with open('video_ids_v5_pruned_pruned_data.json', 'w') as datafile:
 	# 	json.dump(videos_data, datafile, indent = 4, ensure_ascii=True)
