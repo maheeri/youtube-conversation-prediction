859211a39c8406191363b5b75ca6ad21875f90eb
nwparker <nwp7@cornell.edu>
2015-05-01 23:49:02 -0400

    replaced recursion with while loop :sadface:

--- data_collection/video_data.py
+++ data_collection/video_data.py
@@ -23,32 +23,30 @@ DEVELOPER_KEY = "AIzaSyBt4F-EsA1jsLMWERsivgufl0Wn7nwuZ9o"
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
 
-def get_approx_score(vid_id, score=None, next_page_tok=None):
+def get_approx_score(vid_id):
 	""" Get the approx raw_score of comments from a videoID """
 	##############################################
-	# Check base case
-	if next_page_tok == '' or next_page_tok is None and score is not None:
-		return score
-	# If this is the first call, start data
-	if score is None:
-		score = 0
-	# Get the thread via API call
-	api_results = youtube.commentThreads().list(
-		part 		= "snippet",
-		videoId 	= vid_id,
-		textFormat	= "plainText",
-		maxResults	= 100,
-		pageToken 	= next_page_tok
-	).execute()
+	next_page_tok = None
+	score = 0
+	while next_page_tok != '':
+		# Get the thread via API call
+		api_results = youtube.commentThreads().list(
+			part 		= "snippet",
+			videoId 	= vid_id,
+			textFormat	= "plainText",
+			maxResults	= 100,
+			pageToken 	= next_page_tok
+		).execute()
 
-	# Process the thread and get the associated comments
-	for item in api_results["items"]:
-		num_replies = item["snippet"]["totalReplyCount"]
-		# Only add for 2 or more replies
-		score = score + num_replies if num_replies >= 2 else score
-	# Recur
-	new_next_page_tok = api_results["nextPageToken"] if "nextPageToken" in api_results else ''
-	return get_approx_score(vid_id, score, new_next_page_tok)
+		# Process the thread and get the associated comments
+		for item in api_results["items"]:
+			num_replies = item["snippet"]["totalReplyCount"]
+			# Only add for 2 or more replies
+			score = score + num_replies if num_replies >= 2 else score
+		
+		next_page_tok = api_results["nextPageToken"] if "nextPageToken" in api_results else ''
+		next_page_tok = '' if next_page_tok is None else next_page_tok #condition for while loop sanity
+	return score
 
 
 def get_comment_threads(vid_id, data=None, next_page_tok=None):
@@ -151,12 +149,11 @@ def process_inv_idx(inv_idx, path=None, cautious=True):
 		if vid_id not in vid_id_set:
 			print "proccessing: ", vid_id
 			cur_vid_data = get_video_data(vid_id, categories_list)
-			if cur_vid_data is not None:
-				if cautious:
-					with open(path+vid_id+'.json', 'w') as datafile:
-						json.dump(cur_vid_data, datafile, indent = 4, ensure_ascii=True)
-				else:
-					all_data[vid_id] = cur_vid_data
+			if cautious:
+				with open(path+vid_id+'.json', 'w') as datafile:
+					json.dump(cur_vid_data, datafile, indent = 4, ensure_ascii=True)
+			else:
+				all_data[vid_id] = cur_vid_data
 	return all_data
 
 
@@ -170,7 +167,7 @@ def try_forever():
 
 if __name__ == "__main__":
 	os.chdir(os.path.join(os.pardir, 'data')) #go into data folder
-	input_json_filename = 'video_ids_v5.json'
+	input_json_filename = 'list_v5_p1_1.json'
 	videoIds_inv_idx = json.load(open(input_json_filename))
 	# try_forever()
 	process_inv_idx(videoIds_inv_idx, path="comments_v2/", cautious=True)
