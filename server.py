ebd00f6d52fd7aeb3863801d4928328619644865
Stepheny Cheuk <sc2354@cornell.edu>
2015-05-05 20:34:13 -0400

    frontend stuff

--- server.py
+++ server.py
@@ -1,23 +1,26 @@
+import os, sys
+sys.path.append(os.path.abspath("./model")) 
+
+
 from flask import Flask, render_template, jsonify, request
-from model import find_videos, prepare
+# from model import prepare, find_videos
+import json
 
 app = Flask(__name__)
-tfv, svr = prepare()
+# tfv, svr = prepare()
+
 
 @app.route('/')
 def main():
-    return render_template('youtube_captions.html')
+    return render_template('youtube_new.html')
 
-@app.route('/find_videos', methods=['POST'])
-def ajax_find_restaurants():
-  if request.method == 'POST':
-    query = request.form['query']
-    print query;
-    results = find_videos(query, tfv, svr)
+@app.route('/find_videos')
+def ajax_find_vides():
+  query = request.args.get('query')
 
-    return jsonify({ 'results': results })
-  else:
-    return 'Error'
+  # CALCULATE THINGS NOW
+
+  return jsonify(result=query)
 
 if __name__ == '__main__':
   app.debug = True
